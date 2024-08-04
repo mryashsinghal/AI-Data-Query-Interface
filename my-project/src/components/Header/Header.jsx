25db3c8b9321910633237fb029f4b9d8e7b9b473
Yash Singhal <152426138+mryashsinghal@users.noreply.github.com>
2024-08-04 23:38:43 +0530

    Update Header.jsx

--- my-project/src/components/Header/Header.jsx
+++ my-project/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="fixed top-0 w-full p-4 bg-white shadow-md flex justify-between items-center animate-fadeIn">
 
-            <h1 className="text-xl font-bold">AI Data Query Chat Interface</h1>
+            <h1 className="text-xl font-bold">QueryEase</h1>
             <img src="download.png" alt="" className='w-12 border-4' />
         </header>
     )
